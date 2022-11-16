ddf69bf19b8681f3876d5e6bb90287f869408b5a
Aida_Sutaj <aida_sutaj@epam.com>
2022-11-16 11:47:15 +0100

    NEW: method for calculating distance between coordinates

--- src/main/java/com/github/aidasutaj1/taxispringapp/service/VechileService.java
+++ src/main/java/com/github/aidasutaj1/taxispringapp/service/VechileService.java
@@ -1,7 +1,9 @@
 package com.github.aidasutaj1.taxispringapp.service;
 
 import com.github.aidasutaj1.taxispringapp.api.VechileController;
+import com.github.aidasutaj1.taxispringapp.documents.VechileData;
 import com.github.aidasutaj1.taxispringapp.dto.Signal;
+import com.github.aidasutaj1.taxispringapp.repository.VechileDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +13,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Optional;
+
 @Service
 public class VechileService {
 
     private static final Logger log = LoggerFactory.getLogger(VechileController.class);
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Autowired
+    private VechileDataRepository repository;
 
     public void sendSignalToTopic(Signal signal) {
         ListenableFuture<SendResult<String, Object>> future =
@@ -40,7 +49,35 @@ public class VechileService {
 
     }
 
+    public void sendDistanceInfoToTopic(Signal signal) {
+        Double distance = calculateDistancePassedByVechile(signal);
+        VechileData newVechileData = new VechileData(signal.getVechileId(), signal.getLongtitude(), signal.getLatitude(), distance, LocalDateTime.now());
+        repository.save(newVechileData);
+    }
 
+    private Double calculateDistancePassedByVechile(Signal signal) {
+        Optional<VechileData> optionalVechileData = repository.findById(signal.getVechileId());
+        if (optionalVechileData.isPresent()) {
+            VechileData vechileData = optionalVechileData.get();
+            return  calculateDistanceBetweenTwoCoordinates(vechileData.getLastLantitude(), vechileData.getLastLantitude(), signal.getLatitude(), signal.getLongtitude());
+        }
+        return 0.0;
+    }
+
+    private Double calculateDistanceBetweenTwoCoordinates(Double lat1, Double lon1, Double lat2, Double lon2) {
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0.0;
+        } else {
+            Double theta = lon1 - lon2;
+            Double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+            dist = Math.acos(dist);
+            dist = Math.toDegrees(dist);
+            dist = dist * 60 * 1.1515;
+            //N taken for unit
+            dist = dist * 0.8684;
+            return dist;
+        }
+    }
 
 
 }
