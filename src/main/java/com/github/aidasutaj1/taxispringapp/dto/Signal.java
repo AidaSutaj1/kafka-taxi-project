df4b1add59d0609e025f34203486a9fa4f0bd2a1
Aida_Sutaj <aida_sutaj@epam.com>
2022-11-22 13:03:54 +0100

    ADD: validation message and exception handler controller

--- src/main/java/com/github/aidasutaj1/taxispringapp/dto/Signal.java
+++ src/main/java/com/github/aidasutaj1/taxispringapp/dto/Signal.java
@@ -1,21 +1,21 @@
 package com.github.aidasutaj1.taxispringapp.dto;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 public class Signal {
 
-    @NotNull
+    @NotNull(message = "vehicle id cannot be null")
     private Long vehicleId;
-    @NotNull
-    @Min(value=-180, message="Longitude must be equal or greater than -180")
-    @Max(value=180, message="Longitude must be equal or less than 180")
+
+    @NotNull(message = "Longitude cannot be null")
+    @Min(value = -180, message = "Longitude must be equal or greater than -180")
+    @Max(value = 180, message = "Longitude must be equal or less than 180")
     private Double longitude;
-    @NotNull
-    @Min(value=-90, message="Latitude must be equal or greater than -90")
-    @Max(value=90, message="Latitude must be equal or less than 90")
+
+    @NotNull(message = "Latitude cannot be null")
+    @Min(value = -90, message = "Latitude must be equal or greater than -90")
+    @Max(value = 90, message = "Latitude must be equal or less than 90")
     private Double latitude;
 
     public Long getVehicleId() {
